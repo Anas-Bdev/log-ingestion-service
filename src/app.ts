0560f3c9d6d76dac8e15a99894c47ff54427ea91
Anas <anashaj04@gmail.com>
2026-08-15 21:03:03 +0300

    Add global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import router from "./modules/logs/log.routes.js";
+import { errorHandler } from "./middleware/error-handler.js";
  const app=express();
 app.use(express.json());
 app.use(router)
@@ -8,4 +9,6 @@ app.get("/health",(req,res) => {
         status:"ok"
     });
 });
+
+app.use(errorHandler);
 export default app;
---
+++ src/middleware/error-handler.ts
@@ -0,0 +1,14 @@
+import type { ErrorRequestHandler } from "express";
+
+export const errorHandler:ErrorRequestHandler=(
+    err,
+    req,
+    res,
+    next
+) => {
+    console.error(err);
+
+    res.status(500).json({
+        error:"Internal server error"
+    })
+}
